279b135a4604cb3ceed1b39fbcbc29bba967ee89
le482 <le482153@gmail.com>
2023-10-05 15:12:04 +0700

    build view

---
+++ lib/components/coinitem.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+import '../models/coin.dart';
+
+class CoinItem extends StatelessWidget {
+  final Coin coin;
+  const CoinItem({super.key, required this.coin});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("${coin.rank.toString()} .${coin.name} (${coin.symbol})",
+              style:const TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),),
+              Container(
+                child: coin.is_active==true?
+               const Text("active",style: TextStyle(color: Colors.green,fontSize: 15,fontWeight: FontWeight.bold),)
+               :const Text("active",style: TextStyle(color: Colors.red,fontSize: 15,fontWeight: FontWeight.bold),),
+              )
+    
+              
+            ],
+          ),
+         const Divider(thickness: 1,)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/cointag.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+import '../models/tags.dart';
+
+class CoinTag extends StatelessWidget {
+  final Tags tag;
+  const CoinTag({super.key, required this.tag});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 4.0,
+      runSpacing: 0.0,
+      children: [
+        Chip(
+            padding: const EdgeInsets.all(5),
+            backgroundColor: Colors.green,
+            label: Text(
+              tag.name.toString(),
+              style: const TextStyle(color: Colors.white, fontSize: 10),
+            )),
+      ],
+    );
+  }
+}
